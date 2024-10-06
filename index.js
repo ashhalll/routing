4003c75da972e558c4dbfcf8f835409c0bee66f3
Ashhal <ashhalh33@gmail.com>
2024-10-07 00:16:54 +0500

    initialzied express server

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+import express from "express"
+
+const app = express();
+
+
+app.listen(3000, () =>{
+    console.log("Server running on Port 3000!");
+})
