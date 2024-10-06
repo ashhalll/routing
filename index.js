564742188cb082fcb83d1789c817a0f08799cf20
Ashhal <ashhalh33@gmail.com>
2024-10-07 00:24:46 +0500

    added user check

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+var isUserAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck); 
+
+
+
+
 
 app.listen(3000, () =>{
     console.log("Server running on Port 3000!");
