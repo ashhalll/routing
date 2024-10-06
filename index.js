3dedfd8bad4da83f3f644bedf3e74df34c0c2fda
Ashhal <ashhalh33@gmail.com>
2024-10-07 00:34:13 +0500

    error redirect

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ app.post("/check", (req, res) => {
     if (isUserAuthorised) {
       res.sendFile(__dirname + "/public/secret.html");
     } else {
-      res.sendFile(__dirname + "/public/index.html");
-      //Alternatively res.redirect("/");
+      res.redirect(__dirname + "/public/index.html");
     }
   });
 
